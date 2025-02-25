54000e5c308c140350a9386e82ad6c2b6f7b025b
dev <dev@DESKTOP-G66RQCV>
2025-02-25 05:01:46 +0500

    AssetConfiguration

---
+++ src/Milliygramm.Data/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Milliygramm.Domain.Entities;
+
+namespace Milliygramm.Data.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    { }
+}
