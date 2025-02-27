cc587f1e937a115857880b86f35050918b5bce27
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-02-27 13:47:52 +0500

    Add Web Service

---
+++ src/Milliygramm.Web.Service/Services/Auth/IAuthApiService.cs
@@ -0,0 +1,8 @@
+﻿using Milliygramm.Model.DTOs.Auth;
+
+namespace Milliygramm.Web.Service.Services.Auth;
+
+public interface IAuthApiService
+{
+    Task<LoginViewModel> LoginAsync(LoginModel loginModel);
+}
---
+++ src/Milliygramm.Web.Service/Services/Users/IUserApiService.cs
@@ -0,0 +1,8 @@
+﻿using Milliygramm.Model.DTOs.Users;
+
+namespace Milliygramm.Web.Service.Services.Users;
+
+public interface IUserApiService
+{
+    Task<UserViewModel> CreateAsync(UserCreateModel model);
+}
