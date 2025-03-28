cb625b3c52a1493c7a84dff2a103d36178865393
Javohir Khursanboy <javohirkhursanboyev@gmail.com>
2025-03-28 11:09:34 +0500

    Add AuthenticationStateProvider

---
+++ src/Milliygramm.Web/Authorization/CustomAuthStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Claims;
+using Milliygramm.Model.DTOs.Auth;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace Milliygramm.Web.Authorization;
+
+public class CustomAuthStateProvider(ProtectedLocalStorage localStorage) : AuthenticationStateProvider
+{
+    public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var sessionModel = (await localStorage.GetAsync<LoginViewModel>("sessionState")).Value;
+        var identity = sessionModel == null ? new ClaimsIdentity() : GetClaimsIdentity(sessionModel.Token);
+        var user = new ClaimsPrincipal(identity);
+        return new AuthenticationState(user);
+    }
+
+    public async Task MarkUserAsAuthenticated(LoginViewModel model)
+    {
+        await localStorage.SetAsync("sessionState", model);
+        var identity = GetClaimsIdentity(model.Token);
+        var user = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public async Task MarkUserAsLoggedOut()
+    {
+        await localStorage.DeleteAsync("sessionState");
+        var identity = new ClaimsIdentity();
+        var user = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    private ClaimsIdentity GetClaimsIdentity(string token)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var jwtToken = handler.ReadJwtToken(token);
+        var claims = jwtToken.Claims;
+        return new ClaimsIdentity(claims, "jwt");
+    }
+}
---
+++ src/Milliygramm.Web/Components/Pages/Modals/AppModal.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Milliygramm.Web.Components.Pages.Modals;
+
+public partial class AppModal
+{
+    [Parameter]
+    public RenderFragment Title { get; set; }
+    [Parameter]
+    public RenderFragment Body { get; set; }
+    [Parameter]
+    public RenderFragment Footer { get; set; }
+    private bool showModal = false;
+
+    public void Open()
+    {
+        showModal = true;
+    }
+
+    public void Close()
+    {
+        showModal = false;
+    }
+}
